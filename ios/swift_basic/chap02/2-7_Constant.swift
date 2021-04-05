535bb4e201cfdd294fe072219a94bc88b0bd1990
anifilm <anifilm02@gmail.com>
2021-04-05 16:28:41 +0900

    자료 수정

---
+++ ios/swift_basic/chap02/2-7_Constant.swift
@@ -0,0 +1,9 @@
+// 상수의 선언 및 사용
+let name: String = "yagom"  // 차후 변경하지 않는 값은 상수로 선언합니다.
+var age: Int = 100
+var job = "iOS Programmer"  // 타입 추론 사용
+let height = 181.5
+age = 99
+job = "Writer"
+name = "야곰"
+print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 직업은 \(job)입니다. 비밀이지만, 키는 \(height)센티미터입니다.")
