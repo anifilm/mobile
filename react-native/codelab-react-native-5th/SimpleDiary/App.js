a728d4868b4af8e5f3829f0a44f039605c1bc07e
anifilm <anifilm02@gmail.com>
2021-04-19 18:12:52 +0900

    study react-native 2021-04-19

--- react-native/codelab-react-native-5th/SimpleDiary/App.js
+++ react-native/codelab-react-native-5th/SimpleDiary/App.js
@@ -2,14 +2,9 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import List from './pages/List';
+import Detail from './pages/Detail';
+import Form from './pages/Form';
 
 const Stack = createStackNavigator();
 
@@ -17,7 +12,9 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="List" component={List} />
+        <Stack.Screen name="Detail" component={Detail} />
+        <Stack.Screen name="Form" component={Form} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ react-native/codelab-react-native-5th/SimpleDiary/pages/Detail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+
+function Detail({ navigation }) {
+  return (
+    <View>
+      <Text>Detail</Text>
+      <Button title="목록으로" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+export default Detail;
