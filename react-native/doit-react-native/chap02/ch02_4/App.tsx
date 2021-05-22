0541963f2478e5afbd2c19144fc89b60fdac2f7e
anifilm <anifilm02@gmail.com>
2021-05-22 15:54:57 +0900

    Update App.tsx

--- react-native/doit-react-native/chap02/ch02_4/App.tsx
+++ react-native/doit-react-native/chap02/ch02_4/App.tsx
@@ -1,6 +1,28 @@
 import React from 'react'
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, Alert, Button, TextInput } from 'react-native'
+import { TouchableOpacity, TouchableHighlight, Text } from 'react-native'
 
+const onPress = () => Alert.alert('home pressed.', 'message')
+
+// 터처블 컴포넌트 추가
 export default function App() {
-  return <SafeAreaView />
+  return (
+    <SafeAreaView>
+      <Button title="Home" onPress={onPress} />
+      <TouchableOpacity onPress={onPress}>
+        <Text>TouchableOpacity</Text>
+      </TouchableOpacity>
+      <TouchableHighlight onPress={onPress}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
+      {/* Text와 TextInput 추가 */}
+      <TextInput
+        placeholder="enter your name"
+        onChangeText={(text: string) => console.log(text)}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onEndEditing={() => console.log('onEndEditing')}
+      />
+    </SafeAreaView>
+  )
 }
