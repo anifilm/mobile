d77a3599d392cf61f5f98bd0c8d33af6d75267d8
anifilm <anifilm02@gmail.com>
2021-11-01 20:03:24 +0900

    study pwa 2021-11-01

--- _study/doit_pwa/pwa-practice/ex09-todo-app/src/App.vue
+++ _study/doit_pwa/pwa-practice/ex09-todo-app/src/App.vue
@@ -1,23 +1,113 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
+    <!-- 전체영역을 카드 UI로 변경하여 색상의 일관성 유지 -->
+    <v-card>
+      <v-app-bar dark color="primary">
+        <!-- 좌측에 메뉴 아이콘 넣음 -->
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title>To-Do 리스트</v-toolbar-title>
+      </v-app-bar>
+      <v-content>
+        <v-container>
+          <v-row my-5>
+            <v-col cols="7" offset="2">
+              <v-text-field label="할 일을 입력하세요." v-model="sTodoTitle" v-on:keyup.enter="fnSubmitTodo()">
+              </v-text-field>
+            </v-col>
+            <v-col cols="2" my-2>
+              <v-btn
+                fab
+                max-height="50px"
+                max-width="50px"
+                color="pink"
+                dark
+                v-on:click="fnSubmitTodo()"
+              >
+                <v-icon>add</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
 
-    <v-content>
-      <HelloWorld />
-    </v-content>
+          <v-row>
+            <v-col cols="12">
+              <v-list two-line v-for="item in oTodos" :key="item.key">
+                <!-- item.b_edit값을 통해 읽기모드인 경우만 표시 -->
+                <v-card flat color="grey lighten-3" v-if="!item.b_edit">
+                  <!-- 항목을 하나씩 가져와서 tile 단위로 표시함 -->
+                  <v-list-item class="py-2">
+                    <v-list-item-action>
+                      <!-- 체크박스 표시하고 선택되면 변경상태 DB에 저장 -->
+                      <v-checkbox
+                        v-model="item.b_completed"
+                        v-on:change="fnCheckboxChange(item)"
+                      ></v-checkbox>
+                    </v-list-item-action>
+                    <!-- 제목을 표시하고 체크선택되면 취소선 표시 -->
+                    <v-list-item-content>
+                      <v-list-item-title
+                        v-bind:class="{ style_completed: item.b_completed }"
+                      >
+                        {{ item.todo_title }}
+                      </v-list-item-title>
+                      <!-- 두번째 줄에 아이콘 배치 -->
+                      <v-list-item-subtitle class="mt-2">
+                        <!-- 수정 아이콘 표시하고 클릭하면 수정모드로 변경함 -->
+                        <v-icon
+                          class="pointer ml-1"
+                          v-on:click="fnSetEditTodo(item['.key'])"
+                          color="blue darken-2"
+                        >create</v-icon>
+                        <!-- 삭제 아이콘 표시하고 클릭하면 해당 item 삭제 -->
+                        <v-icon
+                          class="pointer ml-3"
+                          v-on:click="fnRemoveTodo(item['.key'])"
+                          color="pink"
+                        >delete</v-icon>
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-card>
+                <!-- item.b_edit값을 통해 수정모드인 경우 어둡게 표시 -->
+                <v-card v-else dark>
+                  <v-list-item class="py-2">
+                    <v-list-item-action>
+                      <v-checkbox v-model="item.b_completed"></v-checkbox>
+                    </v-list-item-action>
+                    <!-- v-list-item안에서 텍스트입력과 버튼 사용을 위해
+										v-card 엘리먼트 사용 -->
+                    <v-card-text>
+                      <!-- 포커스를 입력창으로 바로 옮기고 삭제 아이콘도 추가함 -->
+                      <v-text-field
+                        autofocus
+                        clearable
+                        v-model="item.todo_title"
+                        v-on:keyup.enter="fnSaveEdit(item)"
+                        v-on:keyup.esc="fnCancelEdit(item['.key'])"
+                      ></v-text-field>
+                    </v-card-text>
+                    <v-card-actions>
+                      <!-- 수정모드에서 '저장' 아이콘 클릭하면 해당 item 저장 -->
+                      <v-icon
+                        class="pointer"
+                        v-on:click="fnSaveEdit(item)"
+                        color="yellow" large
+                      >save</v-icon>
+                      <!-- 수정모드에서 '취소' 아이콘 클릭하면 취소하고 읽기모드로
+										 돌아감 -->
+                      <v-icon
+                        class="pointer ml-5"
+                        v-on:click="fnCancelEdit(item['.key'])"
+                        color="pink" large
+                      >cancel</v-icon>
+                    </v-card-actions>
+                  </v-list-item>
+                </v-card>
+              </v-list>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </v-card>
   </v-app>
 </template>
 
@@ -91,5 +181,6 @@ export default {
 .style_completed {
   /* 할 일의 제목을 취소선으로 변경 */
   text-decoration: line-through;
+  color: gray;
 }
 </style>
