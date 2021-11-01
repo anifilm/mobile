4f5a6e6fbf8bf4193ddad16a05f9ae69f2639312
anifilm <anifilm02@gmail.com>
2021-11-01 14:57:26 +0900

    study pwa 2021-11-01

--- _study/doit_pwa/pwa-practice/ex09-todo-app/src/App.vue
+++ _study/doit_pwa/pwa-practice/ex09-todo-app/src/App.vue
@@ -16,21 +16,80 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// 파이어베이스 DB 가져옴
+import { oTodosinDB } from '@/datasources/firebase';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      oTodos: [], // 할일 데이터 목록 저장 변수
+      sTodoTitle: '' // 할 일 제목 저장 문자열 변수
+    };
   },
-  data: () => ({
-    //
-  }),
+  // 파이어베이스를 쉽게 사용하도록 oTodos 변수로 변경
+  firebase: {
+    oTodos: oTodosinDB
+  },
+  methods: {
+    // 할 일 제목, 완료, 수정모드 상태값을 DB에 저장
+    fnSubmitTodo() {
+      oTodosinDB.push({
+        todo_title: this.sTodoTitle,
+        b_completed: false,
+        b_edit: false
+      });
+      this.sTodoTitle = '';
+    },
+    // 전달된 할 일을 DB에서 제거
+    fnRemoveTodo(pKey) {
+      oTodosinDB.child(pKey).remove();
+    },
+    // 전달된 할 일의 b_edit를 수정모드로 변경
+    fnSetEditTodo(pKey) {
+      oTodosinDB.child(pKey).update({
+        b_edit: true
+      });
+    },
+    // 전달된 할 일의 b_edit를 읽기모드로 변경
+    fnCancelEdit(pKey) {
+      oTodosinDB.child(pKey).update({
+        b_edit: false
+      });
+    },
+    // 전달된 할 일의 수정값을 DB에 저장
+    fnSaveEdit(pItem) {
+      const sKey = pItem['.key'];
+      oTodosinDB.child(sKey).set({
+        todo_title: pItem.todo_title,
+        b_completed: pItem.b_completed,
+        b_edit: false
+      });
+    },
+    // 체크박스 선택되면 DB에 b_completed 변경값 저장
+    fnCheckboxChange(pItem) {
+      const sKey = pItem['.key'];
+      oTodosinDB.child(sKey).update({
+        b_completed: pItem.b_completed
+      });
+    }
+  }
 };
 </script>
+
+<style>
+.pointer {
+  /* 마우스포인터를 손모양으로 변경 */
+  cursor: pointer;
+}
+.style_completed {
+  /* 할 일의 제목을 취소선으로 변경 */
+  text-decoration: line-through;
+}
+</style>
